a51045f427b97650abbeb1f980dd2930886e892d
Theo Sun <theo.sun.github@outlook.com>
2023-03-21 19:53:21 +0800

    feat: update env config

--- src/db.ts
+++ src/db.ts
@@ -7,6 +7,7 @@ import * as Config from './config';
 export const connect = async (
   log?: (level: LogLevel, message: string, req?: Request, err?: Error) => void
 ): Promise<void> => {
+
   // Set the db connection options from config settings
   const options: mongoose.ConnectionOptions = {
     connectTimeoutMS: Config.get().db.connTimeout,
@@ -17,17 +18,20 @@ export const connect = async (
     useUnifiedTopology: true,
   };
 
+  const host =  process.env.XBROWSERSYNC_DB_HOST ?? Config.get().db.host
+  
   // Configure db credentials
-  const username = Config.get().db.username || process.env.XBROWSERSYNC_DB_USER;
-  const password = Config.get().db.password || process.env.XBROWSERSYNC_DB_PWD;
+  const username = process.env.XBROWSERSYNC_DB_USER ?? Config.get().db.username
+  const password = process.env.XBROWSERSYNC_DB_PWD ?? Config.get().db.password 
   const creds = username && password ? `${encodeURIComponent(username)}:${encodeURIComponent(password)}@` : '';
+
 
   // Create mongo connection uri using host and db name defined in config settings
   let dbServerUrl = 'mongodb';
   if (Config.get().db.useSRV) {
-    dbServerUrl += `+srv://${creds}${Config.get().db.host}/${Config.get().db.name}`;
+    dbServerUrl += `+srv://${creds}${host}/${Config.get().db.name}`;
   } else {
-    dbServerUrl += `://${creds}${Config.get().db.host}:${Config.get().db.port}/${Config.get().db.name}`;
+    dbServerUrl += `://${creds}${host}:${Config.get().db.port}/${Config.get().db.name}`;
   }
   dbServerUrl += Config.get().db.authSource ? `?authSource=${Config.get().db.authSource}` : '';
 

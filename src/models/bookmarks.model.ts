e4777688833036a47ab34a4f0fa0d850386b2881
Theo Sun <theo.sun@sap.com>
2024-11-23 13:05:10 +0800

    chore: dep update

--- src/models/bookmarks.model.ts
+++ src/models/bookmarks.model.ts
@@ -1,6 +1,3 @@
-import { randomUUID } from 'crypto';
-import mongoose from 'mongoose';
-
 // Interface for bookmarks model
 export interface IBookmarks {
   _id?: any;
@@ -9,37 +6,3 @@ export interface IBookmarks {
   lastUpdated?: Date;
   version?: string;
 }
-
-// Interface for bookmarks mongoose model
-export interface IBookmarksModel extends IBookmarks, mongoose.Document {
-  _id: any;
-}
-
-// Create bookmarks schema to store bookmarks sync data
-// Store IDs as binary uuid v4 and disable default id properties
-// No concurrent updates so disable version keys
-const bookmarksSchema = new mongoose.Schema(
-  {
-    _id: {
-      type: mongoose.Schema.Types.String,
-      default: () => randomUUID().replace(/-/g, ''),
-    },
-    bookmarks: String,
-    lastAccessed: {
-      default: Date,
-      type: Date,
-    },
-    lastUpdated: {
-      default: Date,
-      type: Date,
-    },
-    version: String,
-  },
-  {
-    _id: false,
-    id: false,
-    versionKey: false,
-  }
-);
-
-export const BookmarksModel = mongoose.model<IBookmarksModel>('Bookmark', bookmarksSchema, 'bookmarks');
--- src/models/newSyncLogs.model.ts
+++ src/models/newSyncLogs.model.ts
@@ -1,6 +1,4 @@
-import moment from 'moment';
 import mongoose from 'mongoose';
-import * as uuid from 'uuid';
 
 // Interface for new sync log model
 export interface INewSyncLog {
@@ -10,25 +8,3 @@ export interface INewSyncLog {
 }
 
 export interface INewSyncLogsModel extends INewSyncLog, mongoose.Document {}
-
-// Create new sync logs schema to store ip address and created date
-// No concurrent updates so disable version keys
-const newSyncLogsSchema = new mongoose.Schema(
-  {
-    _id: { type: String, default: uuid.v4 },
-    expiresAt: {
-      default: () => moment().add(1, 'days').startOf('day').toDate(),
-      type: Date,
-    },
-    ipAddress: String,
-    syncCreated: {
-      default: () => new Date(),
-      type: Date,
-    },
-  },
-  {
-    versionKey: false,
-  }
-);
-
-export const NewSyncLogsModel = mongoose.model<INewSyncLogsModel>('NewSyncLog', newSyncLogsSchema, 'newsynclogs');
